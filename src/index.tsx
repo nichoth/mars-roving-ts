97e62f33f68f828639743463847e0edbe3658c72
nick thomas <nichoth@gmail.com>
2022-05-25 22:01:34 -0700

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Provider } from 'react-redux'
+import store from './app/store'
+
 import "./index.css"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div className="test">hello world</div>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <div className="test">hello world</div>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 )
+
+  // <React.StrictMode>
+  //   <div className="test">hello world</div>
+  // </React.StrictMode>,
